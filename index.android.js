5df8d272793b56ea4849d7d9f4e5f1cea639aba6
Nidhishah123 <lanetteam.nidhis@gmail.com>
2017-08-02 18:07:16 +0530

    Remove class from index.android and index.ios files

--- index.android.js
+++ index.android.js
@@ -8,12 +8,4 @@ import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 
-export default class one_time_password extends Component {
-  render() {
-    return (
-      <App/>
-    );
-  }
-}
-
-AppRegistry.registerComponent('one_time_password', () => one_time_password);
+AppRegistry.registerComponent('one_time_password', () => App);
