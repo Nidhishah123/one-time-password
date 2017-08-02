d8f78046abe9650f6b7be17d090979efa2b3854f
Nidhishah123 <lanetteam.nidhis@gmail.com>
2017-08-02 14:53:33 +0530

    Initial project setup

---
+++ index.android.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+
+export default class one_time_password extends Component {
+  render() {
+    return (
+      <App/>
+    );
+  }
+}
+
+AppRegistry.registerComponent('one_time_password', () => one_time_password);
---
+++ src/App.js
@@ -0,0 +1,17 @@
+/**
+ * Created by lcom71 on 2/8/17.
+ */
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+class App extends Component {
+    render(){
+        return(
+            <View>
+
+            </View>
+        );
+    }
+}
+
+export default App;
