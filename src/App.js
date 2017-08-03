eb490a9290560151af88677f9566bf8d3a0bf6f4
Nidhishah123 <lanetteam.nidhis@gmail.com>
2017-08-03 18:21:19 +0530

    Integrate firebase with app

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@
  */
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import firebase from 'firebase';
 import SignUpForm from './SignUpForm';
 import LoginForm from './LoginForm';
 
 class App extends Component {
+
+    componentDidMount() {
+        const config = {
+            apiKey: "AIzaSyB_K00djhwRWp0WxZIvU-MRmk7r45uvreM",
+            authDomain: "one-time-password-4c434.firebaseapp.com",
+            databaseURL: "https://one-time-password-4c434.firebaseio.com",
+            projectId: "one-time-password-4c434",
+            storageBucket: "one-time-password-4c434.appspot.com",
+            messagingSenderId: "754439058760"
+        };
+        firebase.initializeApp(config);
+    }
+
     render(){
         return(
             <View style={styles.rootContainer}>
